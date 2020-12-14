bc5cedf62403819b552ad1ceee3d2dc629a29db5
Yiwei Li <leepoly2@gmail.com>
2020-12-14 15:07:23 +0800

    [gitignore] adjust gitignore

--- env.sh
+++
@@ -1,4 +0,0 @@
-export PATH=$PATH:/usr/local/go/bin # add Go path
-export PATH=$PATH:/home/fabric/phd-2/cybernet/creditdb/bin # add peer path
-go env -w GO111MODULE=on
-go env -w GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
