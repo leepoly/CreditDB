03607bcaede173055d8b358fb9ef5cb7c6d88186
Yiwei Li <leepoly2@gmail.com>
2020-12-15 10:16:36 +0800

    [env] add a sample env.sh for golang configuration

---
+++ env.sh
@@ -0,0 +1,4 @@
+export PATH=$PATH:/home/liyiwei/tools/go/bin # add Go path
+# export PATH=$PATH:/home/liyiwei/phd-2/cybernet/creditdb/bin # add peer path
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
